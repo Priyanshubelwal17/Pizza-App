ccdd37361f64952e6f1030079e39786da379cb34
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-27 23:47:38 +0530

    Learn the box model: spacing borders,and display

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="font-semihold text-sm">jonas</div>;
+  return <div className="font-semihold hidden text-sm">jonas</div>;
 }
 
 export default Username;

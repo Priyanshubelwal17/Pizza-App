1f1139629acabccebcdfc7820d2e43b38b4b04ff
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-30 21:04:33 +0530

    Setup Redux store and added user slice

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="font-semihold hidden text-sm md:block">Priyanshu</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="font-semihold hidden text-sm md:block">{username}</div>
+  );
 }
 
 export default Username;
